f6383f63d29ebdbd4ab3d30ee9a2a57ea1e213b3
understand-jin <sotorl0005@gmail.com>
2024-06-18 21:56:26 +0900

    done

--- stacking_relocation_count.py
+++ stacking_relocation_count.py
@@ -3,32 +3,32 @@ import os
 import pandas as pd
 
 def main():
-    input_dir = 'C:\\Users\\user\\OneDrive\\바탕 화면\\stacking_non_relocation\\Stacking_container\\experiment\\Output_Data_25\\Heiristic_1\\Initial_0\\New_25'
+    input_dir = 'C:\\Users\\user\\OneDrive\\바탕 화면\\stacking_non_relocation\\Stacking_container\\experiment\\Output_Data_25\\Heuristic_1\\Initial_5\\New_20'
 
-    input_files = sorted(glob.glob(os.path.join(input_dir, 'Configuration_*.csv')))
+    input_files = sorted(glob.glob(os.path.join(input_dir, 'Configuration_ex*.csv')))
+    print(f'Found{len(input_files)} files')
 
     total_relocation = 0
+
     total_num = len(input_files)
 
-    for i in range(len(input_files)):
-        input_path = input_files[i]
+    for input_path in input_files:
+        print(f'Processing file : {input_path}')
 
         input_df = pd.read_csv(input_path)
 
         relocation = input_df['reloc'].tolist()
 
-        num = input_df['weight'].tolist()
+        total_relocation += sum(relocation)
 
-        total_relocation = total_relocation + sum(relocation)
-
-    
     print(f'total_relocation : {total_relocation}')
     print(f'total_num : {total_num}')
 
-    if total_relocation != 0:
-       mean_relocation = total_relocation / total_num
-       print(f'mean_relocation : {mean_relocation}')
-    
+    if total_num > 0:
+        mean_relocation = total_relocation / total_num
+        print(f'mean_relocation : {mean_relocation}')
+
+
 main()
 
 
